bb753bb063e5d59cd42bd9ba0b82dc6f8f9e6532
F-Hamid <hamid@hamids-MacBook-Pro.local>
2024-05-25 18:25:06 +0100

    header / auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-import { Button } from "@nextui-org/react";
-import * as actions from "@/actions";
-import { auth } from "@/auth";
-import Profile from "@/components/profile";
-
-export default async function Home() {
-  const session = await auth();
-  console.log(session);
-  return (
-    <div className="w-full h-screen center flex-col">
-      <form action={session?.user ? actions.signOut : actions.signIn}>
-        <Button type="submit">
-          {session?.user ? "Sign out!" : "Sign in!"}
-        </Button>
-      </form>
-      <Profile />
-    </div>
-  );
+export default function Home() {
+  return <div className="w-full h-screen center flex-col">Home Page</div>;
 }
